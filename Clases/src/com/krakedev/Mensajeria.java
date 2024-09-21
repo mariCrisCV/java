a7514c9023ed018576f9ad00298dca841fc1c66a
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-09-21 15:35:18 -0500

    funcion mensajeria ejemplo de funciones que no retornan

---
+++ Clases/src/com/krakedev/Mensajeria.java
@@ -0,0 +1,15 @@
+package com.krakedev;
+
+public class Mensajeria {
+	public void saludar() {
+		System.out.println("Hola HUMANOS");
+	}
+	public void saludarAmigo(String nombre, String apellido) {
+		System.out.println("Hola "+nombre+" "+apellido);
+	}
+}
+
+
+
+
+
