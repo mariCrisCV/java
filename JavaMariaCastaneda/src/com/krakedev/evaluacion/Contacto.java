e87072b4c29abe2a9863ab5c0209728533118fb9
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-10-03 10:03:18 -0500

    requerimiento 4 y 5 ok

--- JavaMariaCastaneda/src/com/krakedev/evaluacion/Contacto.java
+++ JavaMariaCastaneda/src/com/krakedev/evaluacion/Contacto.java
@@ -1,15 +1,19 @@
 package com.krakedev.evaluacion;
 
+import java.util.ArrayList;
+
 public class Contacto {
 	private String cedula;
 	private String nombre;
 	private String apellido;
 	private Direccion direccion;
+	private ArrayList<Telefono> telefonos;
 
 	public Contacto(String cedula, String nombre, String apellido) {
 		this.cedula = cedula;
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.telefonos = new ArrayList<>();
 	}
 
 	public void imprimir() {
@@ -19,6 +23,25 @@ public class Contacto {
 		} else {
 			System.out.println("Dirección: " + direccion.getCallePrincipal() + " y " + direccion.getCalleSecundaria());
 		}
+	}
+
+	public void agregarTelefono(Telefono telefono) {
+		if (!telefono.equals(null) && telefono.getEstado().equals("C")) {
+			telefonos.add(telefono);
+		}
+	}
+
+	public void mostrarTelefonos() {
+		Telefono elementoTelefono;
+		System.out.println("Teléfonos con estado 'C':");
+		for (int i = 0; i < telefonos.size(); i++) {
+			elementoTelefono = telefonos.get(i);
+			if (elementoTelefono.getEstado().equals("C")) {
+				System.out.println(
+						"Numero: " + elementoTelefono.getNumero() + ", " + "Tipo: " + elementoTelefono.getTipo());
+			}
+		}
+
 	}
 
 	public String getCedula() {
@@ -51,6 +74,14 @@ public class Contacto {
 
 	public void setDireccion(Direccion direccion) {
 		this.direccion = direccion;
+	}
+
+	public ArrayList<Telefono> getTelefonos() {
+		return telefonos;
+	}
+
+	public void setTelefonos(ArrayList<Telefono> telefonos) {
+		this.telefonos = telefonos;
 	}
 
 }
