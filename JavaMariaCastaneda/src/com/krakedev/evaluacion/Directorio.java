a71d717936ac0bd9d06697dea3b9a7aa9052175a
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-10-03 12:54:36 -0500

    requerimiento 9

--- JavaMariaCastaneda/src/com/krakedev/evaluacion/Directorio.java
+++ JavaMariaCastaneda/src/com/krakedev/evaluacion/Directorio.java
@@ -1,19 +1,22 @@
 package com.krakedev.evaluacion;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Directorio {
 	private ArrayList<Contacto> contactos;
-	private Date fechaModificacion;
+	private Date ultimaModificacion;
 
 	public Directorio() {
 		contactos = new ArrayList<>();
+		ultimaModificacion =null;
 	}
 
 	public boolean agregarContacto(Contacto contacto) {
 		if (contacto != null && buscarPorCedula(contacto.getCedula()) == null) {
 			contactos.add(contacto);
+			ultimaModificacion=new Date();
 			return true;
 		}
 			return false;		
@@ -30,6 +33,14 @@ public class Directorio {
 			}
 		}
 		return null;
+	}
+	
+	public String consultarUltimaModificacion() {
+		if(ultimaModificacion ==null) {
+			return "No se ha realizado ninguna modificacion";
+		}
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/mm/dd HH:mm:ss");
+		return sdf.format(ultimaModificacion);
 	}
 
 }
